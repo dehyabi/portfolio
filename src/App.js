f04c433db56d3bc21edf54ef0473ad03f48b0b3e
dehyabi <dehyasvibe@gmail.com>
2024-01-31 05:23:27 +0700

    under construction

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import uc_img from './Components/Assets/uc.png'
+
+function App() {
+  return (
+    <div className="App">
+    <h3>Portfolio of Dehya Qalbi</h3>
+
+    <img src={uc_img} />
+    <p>Sorry, this website is under construction</p>
+
+    </div>
+  );
+}
+
+export default App;
