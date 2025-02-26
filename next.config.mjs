393689a27e63522ba80b133c10a3d0056f58f0fb
Dehya Qalbi <dehyasvibe@gmail.com>
2025-02-27 02:17:58 +0700

    setup for github page

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,12 @@ const nextConfig = {
 
     return config;
   },
+  output: 'export', 
+  images: {
+    unoptimized: true 
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '', 
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '', 
 };
 
 export default nextConfig;
